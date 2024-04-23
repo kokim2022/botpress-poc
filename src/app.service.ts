faaacd3cd7c14dca90593c81687a2b16c57f5aef
hwaung <102525421+kokim2022@users.noreply.github.com>
2024-04-23 14:06:24 +0630

    logger added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    this.logger.debug('running bot press poc');
     return 'running bot press poc';
+  }
+
+  webhook(body) {
+    this.logger.debug('body', body);
   }
 }
